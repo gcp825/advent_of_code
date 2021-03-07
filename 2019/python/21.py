4d7caa60c8a62f3726bb99c70fe8742de846f0cf
Chris C <62497020+gcp825@users.noreply.github.com>
2021-03-07 09:22:51 +0000

    Add commentary

--- 2019/python/21.py
+++ 2019/python/21.py
@@ -1,3 +1,10 @@
+#  Worst 'puzzle' ever. Not just obtusely written, but blatantly untrue in places (the Springdroid DOES run Intcode - just IntCode generated from
+#  SpringScript) and there's no logical reason why ['NOT A J','OR D J','WALK'] i.e. just keep jumping whenever you have to or won't die isn't valid
+#  for Part 1 other than the fact that it doesn't give the right answer. 
+
+#  I assume the solution is actually predicated on NOT jumping unless necessary, but there's not even an implication of that being the case in the
+#  description, let alone that being explicit (I've reread it several times). Just... bad.
+
 from intcode import IntcodeComputer    # see intcode.py in this repo
 
 def explore(f,instr):
