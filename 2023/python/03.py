36001c7b04e4fffa08b7948e2baa37a1a3a0f46b
Chris C <62497020+gcp825@users.noreply.github.com>
2023-12-03 17:00:21 +0000

    2023 Day 03

--- 2023/python/03.py
+++ 2023/python/03.py
@@ -1,30 +1,35 @@
 #  The code for these grid coordinate puzzles always ends up looking horrible. Too many different x and y variables 
 #  and you can't call them anything else because they are literally references to x and y axes.
 
-def extract_parts_and_gears(grid):
+def get_parts_and_gears(grid):
 
-    parts = [];  gears = dict();  max_y = len(grid)-1;  max_x = len(grid[0])-1;  bounds = (max_y, max_x)
+    parts = [];  possible_gears = dict();  max_y = len(grid)-1;  max_x = len(grid[0])-1
 
     for y in range(max_y+1):
 
         x = 0
         while x <= max_x:
 
-            if not grid[y][x].isnumeric(): x += 1
+            if not grid[y][x].isdigit(): x += 1
             else:
-                x1 = x
-                while x <= max_x and grid[y][x].isnumeric(): x += 1
-
-                adjacent_symbols = get_symbols(grid,y,x1,x-1,bounds)
+                x, coords = get_number_coords(grid,y,x,max_x)
+                adjacent_symbols = get_symbols(grid,*coords,(max_y,max_x))
 
                 if adjacent_symbols:
-                    new_part = int(grid[y][x1:x])
-                    parts += [new_part]
+                    parts += [int(grid[coords[0]][coords[1]:coords[2]+1])]
                     for coords in [c for s,c in adjacent_symbols if s == '*']:
-                        gears[coords] = gears.get(coords,[]) + [new_part]
+                        possible_gears[coords] = possible_gears.get(coords,[]) + parts[-1:]
 
-    return parts, gears
+    return parts, [p for p in possible_gears.values() if len(p) == 2]
 
+
+def get_number_coords(grid,y,x1,max_x):
+
+    x2 = x1
+    while x2 <= max_x and grid[y][x2].isdigit(): x2 += 1
+
+    return x2, (y,x1,x2-1)
+    
 
 def adjacent_coords(y,x1,x2):
 
@@ -35,14 +40,14 @@ def get_symbols(grid,y,x1,x2,bounds):
 
     coords = [(yy,xx) for yy,xx in adjacent_coords(y,x1,x2) if 0 <= yy <= bounds[0] and 0 <= xx <= bounds[1]]
     
-    return [(s,c) for s,c in [(grid[yy][xx], (yy,xx)) for yy,xx in coords] if s != '.' and not s.isnumeric()]
+    return [(s,c) for s,c in [(grid[yy][xx], (yy,xx)) for yy,xx in coords] if s != '.' and not s.isdigit()]
 
 
 def main(filepath):
 
     grid = [x for x in open(filepath).read().split('\n')]
-    parts, gears = extract_parts_and_gears(grid)
+    parts, gears = get_parts_and_gears(grid)
 
-    return sum(parts), sum([p[0]*p[1] for p in gears.values() if len(p) == 2])
+    return sum(parts), sum([g[0]*g[1] for g in gears])
 
 print(main('03.txt'))
