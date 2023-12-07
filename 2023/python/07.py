7a0552c0cdcdcfece18b9defb58ebbccd9e194b0
Chris C <62497020+gcp825@users.noreply.github.com>
2023-12-07 21:01:30 +0000

    2023 Day 07

--- 2023/python/07.py
+++ 2023/python/07.py
@@ -1,65 +1,44 @@
-# I'm sure this could be much slicker, but limited time for this today, so for now: it is what it is...
-
 from collections import Counter
 
-def parse_input(filepath):
+def parse_input(filepath,jokers=False):
 
-    return [(h,int(b)) for h,b in [x.split(' ') for x in open(filepath).read().split('\n')]]
+    swap = lambda x: x.replace('J','0') if jokers else x
 
-
-def calculate_winnings(hands,jokers=False):
-
-    return sum([(i+1)*b for i,(_,b) in enumerate(sorted([(ranking(cards,jokers),bid) for cards, bid in hands]))])
+    return [(h,int(b)) for h,b in [x.split(' ') for x in swap(open(filepath).read()).split('\n')]]
 
 
-def ranking(cards,jokers):
+def calculate_winnings(hands):
 
-    return (type_rank(cards,jokers), card_order_rank(cards,jokers))
-
-
-def type_rank(cards,jokers):
-
-    counts = sorted([(ct, card) for card, ct in Counter(list(cards)).items()])
-
-    if jokers and 'J' in cards and len(counts) > 1:
-        counts = apply_jokers(counts)
-
-    distinct_cards = len(counts)
-
-    if   distinct_cards == 5: rank = 0
-    elif distinct_cards == 4: rank = 1
-    elif distinct_cards == 3 and counts[2][0] == 2: rank = 2
-    elif distinct_cards == 3 and counts[2][0] == 3: rank = 3
-    elif distinct_cards == 2 and counts[1][0] == 3: rank = 3.2
-    elif distinct_cards == 2 and counts[1][0] == 4: rank = 4
-    elif distinct_cards == 1: rank = 5
-    else:
-        raise ValueError(f"What sort of hand is this?!: {cards}")
-
-    return rank
+    return sum([(i+1)*b for i,(_,b) in enumerate(sorted([(ranking(cards),bid) for cards, bid in hands]))])
 
 
-def apply_jokers(counts):
+def ranking(cards):
 
-    i, jokers = [(i,ct) for i,(ct,c) in enumerate(counts) if c == 'J'][0]
-    no_jokers = counts[:i] + counts[i+1:]
-
-    return no_jokers[:-1] + [(no_jokers[-1][0] + jokers, no_jokers[-1][1])]
+    return (type_rank(cards), card_order_rank(cards))
 
 
-def card_order_rank(cards,jokers):
+def type_rank(cards):
 
-    ranks = dict(zip(list('23456789TJQKA'),range(1,14)))
-    if jokers: ranks['J'] = 0
+    counts = Counter(cards)
+    regular_cards = [x[::-1] for x in sorted([(n,c) for c,n in counts.items() if c != '0'])[::-1]]
+    jokers_applied = [(c,n + counts.get('0',0)) for c,n in regular_cards[:1]] + regular_cards[1:] if regular_cards else [('0',5)]
+
+    return ''.join([str(x[1]) for x in jokers_applied])
+
+
+def card_order_rank(cards):
+
+    ranks = dict(zip(list('023456789TJQKA'),range(14)))
 
     return tuple([ranks[card] for card in cards])
 
 
 def main(filepath):
 
-    hands = parse_input(filepath)
+    jacks = parse_input(filepath)
+    jokers = parse_input(filepath,True)
 
-    return calculate_winnings(hands), calculate_winnings(hands,True)
+    return calculate_winnings(jacks), calculate_winnings(jokers)
 
 
 print(main('07.txt'))
