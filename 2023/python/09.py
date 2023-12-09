c31ddc4d487f094ef1b255d7525b935fd6943f86
Chris C <62497020+gcp825@users.noreply.github.com>
2023-12-09 22:18:44 +0000

    2023 Day 09

--- 2023/python/09.py
+++ 2023/python/09.py
@@ -1,37 +1,26 @@
-def differences(sequence):
-
-    def diff(sequence): return [sequence[i+1]-sequence[i] for i in range(len(sequence)-1)]
+def next_value(sequence):
 
     sequences = [] + [sequence]
-    while not (sum(sequences[-1]) == 0 and len(list(set(sequences[-1]))) == 1):
-        sequences += [diff(sequences[-1])]
 
-    return sequences[::-1]
+    while set(sequences[-1]) != {0}:
+        sequences += [[y-x for y,x in zip(sequences[-1][1:],sequences[-1])]]
 
+    for i in range(len(sequences)-2,-1,-1):
+        sequences[i] += [sequences[i][-1] + sequences[i+1][-1]]
 
-def increase_sequence(sequence):
-
-    sequences = differences(sequence)
-
-    for i in range(1,len(sequences)):
-        sequences[i] += [sequences[i][-1] + sequences[i-1][-1]]
-
-    return sequences[-1]
+    return sequences[0][-1]
 
 
-def sum_final_values(sequences):
+def sum_next_values(sequences):
 
-    return sum([sequence[-1] for sequence in sequences])
+    return sum([next_value(sequence) for sequence in sequences])
 
 
 def main(filepath):
 
     sequences = [list(map(int,x.split(' '))) for x in open(filepath).read().split('\n')]
 
-    part_1 = [increase_sequence(seq) for seq in sequences]
-    part_2 = [increase_sequence(seq) for seq in [s[::-1] for s in sequences]]
-
-    return sum_final_values(part_1), sum_final_values(part_2)
+    return sum_next_values(sequences), sum_next_values([s[::-1] for s in sequences])
 
 
 print(main('09.txt'))
