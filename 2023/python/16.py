ff4865de73a4d73d6d6d42ca8b0acee366e842a6
Chris C <62497020+gcp825@users.noreply.github.com>
2023-12-16 12:59:51 +0000

    2023 Day 16

--- 2023/python/16.py
+++ 2023/python/16.py
@@ -1,50 +1,53 @@
-#  Original version was horrifically slow due to using lists rather than sets.
-#  It's still slow at 12 seconds... might be the way I'm calculating the next moves - which doesn't
-#  use many lines of code but has got to the stage where it feels convoluted and might not be efficient.
-#  Perhaps some more tuning later...
+#  This is slow at ~9 seconds on my laptop... but other than trying to fast forward rather than move
+#  step-by-step or cache intermediate states with their energised counts, not sure what else I can do.
+#  My original move determination process looked much slicker and neater than it does now... but this
+#  implementation is notably quicker and more explicit.
 
-def setup_rules():
+def move(tile,y,x,dir):
 
-    compass = {'n':(-1,0),'s':(1,0),'e':(0,1),'w':(0,-1)}
-    actions = './!|-'
-    directions = ['nesw','enws','wsen',('n','ns','s','ns'),('ew','e','ew','w')]
+    if tile == '/': 
+        return [{'e':(y-1,x,'n'), 'n':(y,x+1,'e'), 'w':(y+1,x,'s'), 's':(y,x-1,'w')}[dir]]
 
-    return dict([(a, dict(zip('nesw',d))) for a,d in zip(actions,directions)]), compass
-           
-       
-def move(grid,next_directions,adjust,y,x,direction):
+    elif tile == '!': 
+        return [{'w':(y-1,x,'n'), 's':(y,x+1,'e'), 'e':(y+1,x,'s'), 'n':(y,x-1,'w')}[dir]]
 
-    return [(y+ay,x+ax,d) for d in tuple(next_directions[grid[y][x]][direction]) for ay,ax in [adjust[d]]]
+    elif tile == '-' and dir in 'ns': 
+        return [(y,x-1,'w'),(y,x+1,'e')]
 
+    elif tile == '|' and dir in 'ew':
+        return [(y-1,x,'n'),(y+1,x,'s')]
 
-def energised(grid,rules,origin):
+    else:
+        return [{'n':(y-1,x,'n'), 'e':(y,x+1,'e'), 's':(y+1,x,'s'), 'w':(y,x-1,'w')}[dir]]
+
+    
+def energised(grid,origin):
 
     yy, xx = len(grid)-1, len(grid[0])-1
     seen_states = set()
     queue = [origin]
 
     while queue:
-        state = queue.pop(0)
-        seen_states.add(state)
-        queue += [m for m in move(grid,*rules,*state) if 0 <= m[0] <= yy and 0 <= m[1] <= xx and m not in seen_states]
+        y,x,d = queue.pop(0)
+        seen_states.add((y,x,d))
+        queue += [m for m in move(grid[y][x],y,x,d) if 0 <= m[0] <= yy and 0 <= m[1] <= xx and m not in seen_states]
 
     return len(list(set([(y,x) for y,x,_ in seen_states])))
 
 
-def most_energised(grid,rules):
+def most_energised(grid):
 
     origins = ([(y,x,d) for y,d in zip((0,len(grid)-1),'sn') for x in range(len(grid[0]))] +
                [(y,x,d) for x,d in zip((0,len(grid[0])-1),'ew') for y in range(len(grid))])
 
-    return max([energised(grid,rules,origin) for origin in origins])
+    return max([energised(grid,origin) for origin in origins])
 
 
 def main(filepath):
 
     grid = open(filepath).read().replace('\\','!').split('\n')
-    rules = setup_rules()
 
-    return energised(grid,rules,(0,0,'e')), most_energised(grid,rules)
+    return energised(grid,(0,0,'e')), most_energised(grid)
 
 
 print(main('16.txt'))
