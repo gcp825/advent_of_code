8eef47db9c88b82ddad7e9793fc98c3be5c2874f
gcp825 <62497020+gcp825@users.noreply.github.com>
2024-12-01 08:00:19 +0000

    2024 Day 01

--- 2024/python/01.py
+++ 2024/python/01.py
@@ -2,9 +2,6 @@ def main(filepath):
 
     one, two = tuple(map(sorted, zip(*[map(int, line.split()) for line in open(filepath).read().split('\n')])))
 
-    pt1 = sum([abs(b-a) for a,b in zip(one, two)])
-    pt2 = sum([a*two.count(a) for a in one])
-
-    return pt1, pt2
+    return sum([abs(b-a) for a,b in zip(one, two)]), sum([a*two.count(a) for a in one])
 
 print(main('01.txt'))
