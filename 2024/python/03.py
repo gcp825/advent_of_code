aa33232d66f0c704f74b194f12291e05deffed9b
gcp825 <62497020+gcp825@users.noreply.github.com>
2024-12-03 20:20:36 +0000

    2024 Day 03

--- 2024/python/03.py
+++ 2024/python/03.py
@@ -1,22 +1,26 @@
-# I have changed: I am no longer allergic to regex. I still don't like it... but it's definitely the easy option here.
+# Brain not functioning at 6.30am this morning:
+# Did Part 1 without regex, as always try and avoid using it unless really necessary.
+# Then threw Part 1 away and wrote a regex solution for Part 1 & 2... and realised much later there was no need to do that.
+# So after another u-turn, here is a nice simple solution for both parts with no regex...
 
-from ast import literal_eval
-from re import findall
+def calculate(text):
 
-def parse(data, conditionals):
-
-    do, do_not, extension = ("do()", "don't()", "|do\(\)|don't\(\)") if conditionals else (chr(0), chr(0), "")
-
-    matches = "".join(findall("mul\([0-9]{1,3},[0-9]{1,3}\)" + extension, data))
-    pairs = [literal_eval(p) for p in "".join([m.split(do_not)[0] for m in matches.split(do)]).split("mul") if p]
-
-    return sum(a*b for a,b in pairs)
-
+    total = 0
+    for i in range(len(text)+1):
+        if text[i:i+4] == "mul(":
+            contents = text[i+4:].split(")")[0]
+            if 3 <= len(contents) <= 7:
+                left, right = tuple(contents.split(",") + [""])[:2]
+                if len(left) <= 3 and len(right) <= 3 and left.isdigit() and right.isdigit():
+                    total += int(left)*int(right)
+    return total
 
 def main(filepath):
 
-    data = open(filepath).read()
+    data = [line.split("don't()") for line in open(filepath).read().split("do()")]
+    keep = calculate(''.join(x[0] for x in data))
+    lose = calculate(''.join(''.join(x[1:]) for x in data))
 
-    return parse(data, False), parse(data, True)
+    return keep+lose, keep
 
 print(main('03.txt'))
