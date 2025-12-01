1e6b116d2a4e9648614d33cfc69cefd96bfa0cdb
Chris C <62497020+gcp825@users.noreply.github.com>
2025-12-01 16:14:37 +0000

    2025 Day 01

--- 2025/python/01.py
+++ 2025/python/01.py
@@ -1,17 +1,17 @@
 def get_code(filepath, method, size=100, code=0):
 
-    dial = [size // 2]
+    dial = size // 2
     rotations = [({'L':-1}.get(line[0],1), int(line[1:])) for line in open(filepath).read().split('\n')]
 
     for sign, amt in rotations:
 
+        whole_rotations = amt // size
         clicks = (amt % size) * sign
-        dial = dial[-1:] + [(dial[-1] + clicks) % size]
 
-        if method == 1 and dial[1] == 0:
-            code += 1
-        elif method == 2:
-            code += (amt // size) + (0 if dial[0] == 0 else 0 if 1 <= (dial[0] + clicks) < size else 1)
+        passed_zero = 0 if dial == 0 else 0 if 1 <= (dial + clicks) < size else 1
+        dial = (dial + clicks) % size
+
+        code += whole_rotations + passed_zero if method == 2 else 1 if dial == 0 else 0
 
     return code
 
