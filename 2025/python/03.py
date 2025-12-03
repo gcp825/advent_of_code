71252e300385525ab78f72e0dcba16d8d6b2357e
Chris C <62497020+gcp825@users.noreply.github.com>
2025-12-03 08:23:32 +0000

    2025 Day 03

---
+++ 2025/python/03.py
@@ -0,0 +1,26 @@
+def parse_input(filepath):
+
+    return [[int(x) for x in line] for line in open(filepath).read().split('\n')]
+
+
+def get_joltage(bank, number_of_batteries):
+
+    joltage, remaining_bank = [], [] + bank
+
+    while number_of_batteries:
+
+        battery = max(remaining_bank[:len(remaining_bank) - number_of_batteries + 1])
+        remaining_bank = remaining_bank[remaining_bank.index(battery)+1:]
+
+        joltage += [battery]
+        number_of_batteries -= 1
+
+    return int(''.join(map(str,joltage)))
+
+
+def main(filepath):
+
+    return tuple(sum(get_joltage(bank,n) for bank in parse_input(filepath)) for n in (2,12))
+
+
+print(main('03.txt'))
