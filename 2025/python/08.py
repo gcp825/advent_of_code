81db0f3cf7d682f54ee9923171b7c2be1ea66d90
Chris C <62497020+gcp825@users.noreply.github.com>
2025-12-08 19:27:57 +0000

    2025 Day 08

--- 2025/python/08.py
+++ 2025/python/08.py
@@ -1,39 +1,35 @@
 from functools import reduce
+from itertools import combinations
+from math import dist
+
 
 def parse_input(filepath):
 
-    return [tuple(map(int,x.split(','))) for x in open(filepath).read().split('\n')]
+    return [tuple(map(int,line.split(','))) for line in open(filepath).read().split('\n')]
 
 
-def euclidean(a,b):
+def pairs_by_distance(boxes):
 
-    return sum([abs(b[0]-a[0])**2, abs(b[1]-a[1])**2, abs(b[2]-a[2])**2])
-
-
-def calculate_distances(boxes):
-
-    return [(euclidean(a,b),a,b) for i,a in enumerate(boxes) for j,b in enumerate(boxes) if i < j]
+    return list(sorted(combinations(boxes,2), key=lambda x: dist(*x)))
 
 
 def join_boxes(circuits,a,b):
 
-    one = circuits.pop([i for i,x in enumerate(circuits) if a in x][0])
-    two = set() if b in one else circuits.pop([i for i,y in enumerate(circuits) if b in y][0])
+    one, two = tuple([c for c in circuits if box in c][0] for box in (a,b))
 
-    return circuits + [one|two]
+    return [c for c in circuits if c not in (one,two)] + [one|two]
 
 
 def solve(boxes):
 
-    circuits, largest = [{x} for x in boxes], [1]
-    distances = [(a,b) for _,a,b in sorted(calculate_distances(boxes))]
+    circuits, largest_3_circuits = [{box} for box in boxes], [1]
 
-    while len(circuits) > 1:
-        box_a, box_b = distances.pop(0)
-        circuits = join_boxes(circuits, box_a, box_b)
-        largest += [reduce(lambda x,y: x*y, list(sorted([len(x) for x in circuits]))[-3:])]
+    for box_a, box_b in pairs_by_distance(boxes):
+        circuits = join_boxes(circuits,box_a,box_b)
+        largest_3_circuits += [reduce(lambda x,y: x*y, list(sorted([len(x) for x in circuits]))[-3:])]
+        if len(circuits) == 1: break
 
-    return largest[1000], box_a[0] * box_b[0]
+    return largest_3_circuits[1000], box_a[0] * box_b[0]
 
 
 def main(filepath):
